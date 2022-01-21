d8890845f4754fb80646b18548dbb792acd210a6
Michael Dever <mdever@live.ca>
2022-01-20 21:42:44 -0500

    requesting days with axios. remove hard coded days object

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -65,9 +48,17 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday')
+  const [days, setDays] = useState([])
   const appointmentsList = appointments.map((appointment) => (
     <Appointment key={appointment.id} {...appointment} />
   ))
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+    .then(response => {
+      setDays([...response.data])
+    })
+  }, [])
   return (
     <main className="layout">
       <section className="sidebar">
