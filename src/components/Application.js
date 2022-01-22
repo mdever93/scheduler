24ef35ac862a647ef3ea8fe06babeb0b28c4a9b7
Michael Dever <mdever@live.ca>
2022-01-21 21:19:23 -0500

    retrieving interviewer data. getInterview helper function

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,14 +4,15 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   const setDay = (day) => setState({ ...state, day });
   // const setDays = (days) => setState(prev => ({ ...prev, days }));
@@ -20,12 +21,14 @@ export default function Application(props) {
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
       axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers'),
     ])
       .then(all => {
         console.log(all[0]); // first
         console.log(all[1]); // second
-      
-        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data}));
+        console.log(all[2]); // second
+
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
       })
       .catch((err) => console.log(err.message));
   }, [state.day])
@@ -33,9 +36,17 @@ export default function Application(props) {
   let dailyAppointments = [];
   dailyAppointments = getAppointmentsForDay(state, state.day)
 
-  const appointmentsList = dailyAppointments.map((appointment) => (
-    <Appointment key={appointment.id} {...appointment} />
-  ))
+  const appointmentsList = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interviewer={interview} />
+    )
+  })
 
   return (
     <main className="layout">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,12 +11,39 @@ export function getAppointmentsForDay(state, day) {
       appointmentsArray = i.appointments;
     }
   }
-  console.log("APPOINTMENTS ARRAY", appointmentsArray);
+  // console.log("APPOINTMENTS ARRAY", appointmentsArray);
   for (const i of appointmentsArray) {
     if (state.appointments[i]) {
       appointments.push(state.appointments[i])
     }
   }
-  console.log("APPOINTMENTS", appointments);
+  // console.log("APPOINTMENTS", appointments);
   return appointments;
+}
+
+export function getInterview(state, interview) {
+  // console.log("STATE: ", state);
+  // console.log("INTERVIEW", interview);
+  if (interview === null) {
+    return null
+  }
+  const output = interview;
+  console.log("OUTPUT:", output);
+  const appointmentsKeys = Object.keys(state.appointments)
+  for (const i of appointmentsKeys) {
+    if (state.appointments[i].interview === null) {
+      continue;
+    }
+    console.log(state.appointments[i]);
+    if (state.appointments[i].interview.interviewer === interview.interviewer) {
+      const interviewersKeys = Object.keys(state.interviewers);
+      for (const i of interviewersKeys) {
+        console.log('HELLO OUTPUT.INTERVIEWER', output.interviewer, i);
+        if (state.interviewers[i].id === output.interviewer) {
+          output.interviewer = state.interviewers[i]
+          return output;
+        }
+      }
+    }
+  }
 }
