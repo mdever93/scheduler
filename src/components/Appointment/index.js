d74b23be956c43098555ce23f113fd93bdb43a07
Michael Dever <mdever@live.ca>
2022-01-23 18:48:35 -0500

    useVisualMode custom hook. Back limit, transition with replace. Using custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,8 @@ import "./styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
   const appointmentTime = () => {
@@ -11,12 +13,25 @@ export default function Appointment(props) {
     }
     return 'No Appointments'
   }
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       {appointmentTime()}
-      <Header time = {props.time} />
-      {props.interview ? <Show student = {props.interview.student} interviewer = {props.interview.interviewer} /> : <Empty />}
-      </article>
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers = {[]} onCancel = {() => back()} />}
+    </article>
 
   )
 }
--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial)
+  // const [mode, setMode] = useState(initial)
   const [history, setHistory] = useState([initial])
-  const transition = (x) => {
-    setMode(x);
-    setHistory([...history, x]);
+  const transition = (x, replace = false) => {
+    setHistory(prev => replace ? [...prev.slice(0, prev.length - 1), x] : [...prev, x]     );
+
+    // setMode(x);
+    // setHistory([...history, x]);
   }
-  const back = () => {
-    history.pop()
-    setMode(history[history.length - 1])
-  }
-  console.log('MODE', mode);
+  function back() {     if (history.length < 2) return;
+    setHistory(prev => [...prev.slice(0, history.length - 1)])}
+  // const back = () => {
+  //   if (history.length > 1) {
+  //     setHistory((prev) => {
+  //       return [...prev.slice(0, history.length - 1)]
+  //     })
+  //     // history.pop()
+  //   }
+  //   setMode(history[history.length - 1])
+  // }
+  console.log('MODE', history[history.length - 1]);
+  console.log('HISTORY', history);
   return {
-    mode,
+    mode: history[history.length - 1],
     transition,
     back
   }
